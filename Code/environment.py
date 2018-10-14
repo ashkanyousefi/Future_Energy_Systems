f29b401569951233920f9297b1d6dddc3ef34320
Danial Alihosseini <danial.alihosseini@gmail.com>
2018-10-14 23:32:31 +0330

    [initial code structuring]

---
+++ Code/environment.py
@@ -0,0 +1,20 @@
+class Environment(object):
+    class State:
+        def __init__(self):
+            pass
+
+    class Action:
+        def __init__(self):
+            pass
+
+    def __init__(self):
+        pass
+
+    def step(self, action: Action):
+        '''
+        Moves environment one step forward based on input action given by the agent
+
+        :param action: The new action
+        :return: (new state: State, reward: Int, is episode finished (done): Boolean)
+        '''
+        pass
