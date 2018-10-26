752777f863350bad9b0033b03b3220d5cbcb2699
Collin Cao <collin@airbears2-10-142-64-39.airbears2.1918.berkeley.edu>
2018-10-26 10:30:25 -0700

    DQN implemented, need parameter tuning

---
+++ Code/SingleDeviceEnv.py
@@ -0,0 +1,113 @@
+# import the required modules
+import numpy as np
+import random
+
+# Class Environment
+
+class Action:
+    def __init__(self, action):
+        self.action = action
+
+
+class Environment:
+    def __init__(self, appliances_number, appliances_consumption, electricity_cost, udc, schedule_start, schedule_stop
+                 , usage_duration, penalty):
+        self.appliances_number = appliances_number
+        self.appliances_consumption = appliances_consumption
+        self.electricity_cost = electricity_cost
+        self.udc = udc
+        self.schedule_start = schedule_start
+        self.schedule_stop = schedule_stop
+        self.usage_duration = usage_duration
+        self.penalty = penalty
+        self.done = False
+        self.time_stamp = 0
+        self.state_accumulation = 0
+        self.episode_rewards = []
+
+    def get_action_shape(self):
+        return 1
+
+    def reset(self):
+        self.appliances_number = 1
+        self.udc = 1
+        self.appliances_consumption = np.random.randint(1, 10) / 10
+        self.schedule_start = np.random.randint(0, 12)
+        self.schedule_stop = np.random.randint(12, 23)
+        self.usage_duration = np.random.randint(1, 12)
+        self.done = False
+        self.time_stamp = 0
+        self.state_accumulation = 0
+        self.episode_rewards = []
+
+    def action_space_sample(self):
+        return random.randint(0, 1)
+
+    def get_obs_shape(self):
+        return len(self.get_obs())
+
+    def get_obs(self):
+        return [self.time_stamp, self.schedule_start, self.schedule_stop, self.state_accumulation, self.usage_duration]
+
+    def reward(self, action):
+
+        reward_function = (1-self.done) * self.penalty + (action*self.electricity_cost[self.time_stamp]*self.appliances_consumption) + self.done*100
+        if len(self.episode_rewards) == 0:
+            self.episode_rewards.append(reward_function)
+        else:
+            self.episode_rewards.append(self.episode_rewards[-1]+reward_function)
+        return reward_function
+
+    def step(self, action):
+
+        #self.sta.state_accumulation += action
+        if action == 1 and self.time_stamp <= self.schedule_stop and self.time_stamp >= self.schedule_start:
+            self.state_accumulation += 1
+
+        self.time_stamp += 1
+        self.done = self.time_stamp == 23 or self.state_accumulation >= self.usage_duration
+        return self.get_obs(), self.reward(action), self.done
+
+def get_random_env():
+
+    appliances_number = 1
+    udc = 1
+    appliances_consumption = np.random.randint(1, 10) / 10
+    electricity_cost = np.array([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 12, 12, 5, 5, 5, 5, 10, 10, 10, 5, 5, 5]) / 10
+    schedule_start = np.random.randint(0, 12)
+    schedule_stop = np.random.randint(12, 23)
+    usage_duration = np.random.randint(1, 12)
+
+    return Environment(appliances_number, appliances_consumption, electricity_cost, udc, schedule_start, schedule_stop, usage_duration, -.2)
+
+
+if __name__ == '__main__':
+    # load_details = np.array(
+    #     [['load number', 'Appliances', 's', 'f', 'l', 'r', 'udc'], [1, 'Lighting', 18, 20, 3, 0.36, 8],
+    #      [2, 'Washing machine', 8, 14, 2, 0.5, 1], [3, 'Cloth dryer', 11, 17, 1, 1.8, 0]
+    #         , [4, 'Dish washer', 14, 19, 2, 1.2, 9], ['Vacuum cleaner', 9, 14, 1, 0.65, 1],
+    #      [6, 'Iron', 6, 9, 1, 1.1, 1], [7, 'Rice cooker', 7, 10, 1, 0.30, 0]])
+    #
+    # electricity_price = np.array([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 12, 12, 5, 5, 5, 5, 10, 10, 10, 5, 5, 5])
+    # print(Environment.time_stamp)
+    appliances_number = 1
+    udc = np.array([1, 1, 1])
+    appliances_consumption = np.array([3, 4, 5])
+    electricity_cost = np.array([5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 12, 12, 5, 5, 5, 5, 10, 10, 10, 5, 5, 5]) / 10
+    schedule_start = [10, 11, 10]
+    schedule_stop = [19, 17, 21]
+    usage_duration = [2, 3, 1]
+    action = 1
+
+    for start, stop, duration, consumption in zip(schedule_start, schedule_stop, usage_duration, appliances_consumption):
+        env = Environment(appliances_number, consumption, electricity_cost, udc, start, stop, duration, -.2)
+    done = False
+
+    while not done:
+        state, reward, done = env.step(action)
+        print("time stamp %s, current %s, needed %s, reward %s, done %s" % (state.time_stamp, state.state_accumulation, usage_duration, reward, done))
+
+
+    # The setting for the Environment class presented in the following
+
+    # The setting for the state class is set in the following
---
+++ Code/run_dqn_SingleDevice.py
@@ -0,0 +1,111 @@
+import argparse
+import os.path as osp
+import random
+import numpy as np
+import tensorflow as tf
+import tensorflow.contrib.layers as layers
+import SingleDeviceEnv
+
+import dqn
+from dqn_utils import *
+
+def model(obs, num_actions, scope, reuse=False):
+    with tf.variable_scope(scope, reuse=reuse):
+        out = obs
+        with tf.variable_scope("action_value"):
+            out = layers.fully_connected(out, num_outputs=64, activation_fn=tf.nn.relu)
+            out = layers.fully_connected(out, num_outputs=64, activation_fn=tf.nn.relu)
+            out = layers.fully_connected(out, num_outputs=num_actions, activation_fn=None)
+
+        return out
+
+def device_optimizer():
+    return dqn.OptimizerSpec(
+        constructor=tf.train.AdamOptimizer,
+        lr_schedule=ConstantSchedule(1e-3),
+        kwargs={}
+    )
+
+def stop_criterion(num_timesteps):
+    def stopping_criterion(env, t):
+        # notice that here t is the number of steps of the wrapped env,
+        # which is different from the number of steps in the underlying env
+        return env.done
+    return stopping_criterion
+
+def exploration_schedule(num_timesteps):
+    return PiecewiseSchedule(
+        [
+            (0, 1),
+            (num_timesteps * 0.1, 0.02),
+        ], outside_value=0.02
+    )
+
+def kwargs():
+    return {
+        'optimizer_spec': device_optimizer(),
+        'q_func': model,
+        'replay_buffer_size': 50000,
+        'batch_size': 32,
+        'gamma': 0.995,
+        'learning_starts': 1000,
+        'learning_freq': 1,
+        'frame_history_len': 1,
+        'target_update_freq': 3000,
+        'grad_norm_clipping': 10,
+        'lander': True
+    }
+
+def learn(env, session, num_timesteps, seed):
+
+    optimizer = device_optimizer()
+    stopping_criterion = stop_criterion(num_timesteps)
+    exploration_schedule = PiecewiseSchedule(
+        [
+            (0, 1),
+            (num_timesteps * 0.1, 0.02),
+        ], outside_value=0.02
+    )
+
+    dqn.learn(
+        env=env,
+        session=session,
+        exploration=exploration_schedule,
+        stopping_criterion=stop_criterion(num_timesteps),
+        double_q=True,
+        **kwargs()
+    )
+    env.close()
+
+def set_global_seeds(i):
+    tf.set_random_seed(i)
+    np.random.seed(i)
+    random.seed(i)
+
+def get_session():
+    tf.reset_default_graph()
+    tf_config = tf.ConfigProto(
+        inter_op_parallelism_threads=1,
+        intra_op_parallelism_threads=1,
+        device_count={'GPU': 0})
+    # since the observations are low-dimensional
+    session = tf.Session(config=tf_config)
+    return session
+
+def get_env(seed):
+    env = SingleDeviceEnv.get_random_env()
+    set_global_seeds(seed)
+
+    return env
+
+def main():
+    # Run training
+    seed = 4565 # you may want to randomize this
+    print('random seed = %d' % seed)
+    env = get_env(seed)
+    session = get_session()
+    set_global_seeds(seed)
+    learn(env, session, num_timesteps=1e4, seed=seed)
+
+if __name__ == "__main__":
+    main()
